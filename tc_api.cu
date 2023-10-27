ef5e45d9d45995db8a9cec964f3a97e619d0a511
Lingqi <zlqi@qq.com>
2023-10-27 20:16:01 +0900

    update tc file

--- tc_api.cu
+++ tc_api.cu
@@ -35,6 +35,7 @@
 // #define K_TOTAL (K * K_TILES)
 #define ILP 4
 //__global__ void WMMAINT8()
+#include "repeat.h"
 using namespace nvcuda;
 #define cudaCheckError() {                                          \
  cudaError_t e=cudaGetLastError();                                 \
@@ -46,35 +47,45 @@ using namespace nvcuda;
 
 
 
-__global__ void WMMAF16TensorCore(double*A, double*B,  double*C, int tile_c, int repeat)
+
+__global__ void WMMAF16TensorCore(double*A, double*B,  double*C, int tile_c)
 {
 
-  wmma::fragment<wmma::matrix_a, M, N, K, double, wmma::row_major> a_frag;
+  wmma::fragment<wmma::matrix_a, M, N, K, double, wmma::row_major> a_frag[ILP];
   wmma::fragment<wmma::matrix_b, M, N, K, double, wmma::col_major> b_frag;
   wmma::fragment<wmma::accumulator, M, N, K, double> c_frag[ILP];
-  int id_warps=blockIdx.x/32;
+  int ix = (blockIdx.x * blockDim.x + threadIdx.x)/32;
+  int iy = (blockIdx.y * blockDim.y + threadIdx.y);
+  int id_warps=threadIdx.x/32;
   #pragma unroll
   for(int i=0; i<ILP;i++)
   {
     wmma::fill_fragment(c_frag[i], 0.0f);
+    wmma::load_matrix_sync(a_frag[i], A +8*(ix) + tile_c*(iy+i)*8, tile_c);
   }
-  
-  wmma::load_matrix_sync(a_frag, A , M);
-  wmma::load_matrix_sync(b_frag, B , N);
-  for(int i=0; i<repeat; i++)
+
+ 
+
+  wmma::load_matrix_sync(b_frag, B +tile_c*(iy)+8*(ix) , 8);
+
+
+  // wmma::load_matrix_sync(a_frag, A+id_warps*tile_c+8*blockIdx.x, tile_c);
+
+  // wmma::load_matrix_sync(b_frag, B+id_warps*8+tile_c*blockIdx.x, 32);
+
   {
-    #pragma unroll
-    for(int i=0; i<ILP;i++)
     {
-      wmma::mma_sync(c_frag[i], a_frag, b_frag, c_frag[i]);
+      repeat1024(_Pragma("unroll")for(int i=0; i<ILP;i++)wmma::mma_sync(c_frag[i], a_frag[i], b_frag, c_frag[i]););
     }
   }
+
   #pragma unroll
   for(int i=0; i<ILP;i++)
   {
-    wmma::store_matrix_sync(C+N*(blockIdx.x+i*gridDim.x) + tile_c*(id_warps), c_frag[i], tile_c, wmma::mem_row_major);
+    wmma::store_matrix_sync(C+N*(blockIdx.x+i*gridDim.x) + tile_c*(id_warps)*8, c_frag[i], tile_c, wmma::mem_row_major);
   }
-}
+  // printf("%d,%d\n",id_warps,threadIdx.x);
+} 
 #include<unistd.h>   
 
 int main(int argc, char const *argv[])
@@ -82,30 +93,30 @@ int main(int argc, char const *argv[])
   cudaError_t cuda_status;
   int sm_count;
   cudaDeviceGetAttribute ( &sm_count, cudaDevAttrMultiProcessorCount,0 );
-  int num_warps=8;
+  int num_warps=4;
   int tilesC=sm_count*ILP*N;
   int sizeofmem=sizeof(double)*16*16;
   int sizeofrmem=sizeofmem*sm_count*ILP*num_warps;
 
-  double*h_mat=(double*)malloc(sizeofmem);
-  double*h_mat2=(double*)malloc(sizeofmem);
+  double*h_mat=(double*)malloc(sizeofrmem);
+  double*h_mat2=(double*)malloc(sizeofrmem);
   double*d_A;
   double*d_B;
   double*d_C;
-  cudaMalloc((void**)&d_A, sizeofmem);
-  cudaMalloc((void**)&d_B, sizeofmem);
+  cudaMalloc((void**)&d_A, sizeofrmem);
+  cudaMalloc((void**)&d_B, sizeofrmem);
   cudaMalloc((void**)&d_C, sizeofrmem);
   double*h_C=(double*)malloc(sizeofrmem);
 
-  for(int i=0; i<16*16; i++)
+  for(int i=0; i<sm_count*ILP*16*16*num_warps; i++)
   {
-    h_mat[i]=i+1;
+    h_mat[i]=i%20+0.1*i;//+1;
   }
 
-  int repeat=10000; 
+  // int repeat=10000; 
   int outrepeat=10000;
-  cudaMemcpy(d_A, h_mat, sizeofmem, cudaMemcpyHostToDevice);
-  cudaMemcpy(d_B, h_mat, sizeofmem, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_A, h_mat, sizeofrmem, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_B, h_mat, sizeofrmem, cudaMemcpyHostToDevice);
   
   float milliseconds = 0;
   nvmlReturn_t result;
@@ -113,13 +124,13 @@ int main(int argc, char const *argv[])
   result=nvmlInit();
   nvmlEnableState_t mode;
   result = nvmlDeviceGetHandleByIndex(0, &device);
-  result=nvmlDeviceGetPowerManagementMode(device, &mode);
+  result = nvmlDeviceGetPowerManagementMode(device, &mode);
 
   #pragma omp parallel num_threads(2)
   {
     if (omp_get_thread_num() == 1)
     {
-      for(int i=0; i<20;i++)
+      for(int i=0; i<10;i++)
       {
         unsigned int power1, power2;
         result=nvmlDeviceGetPowerUsage(device,&power1);
@@ -138,24 +149,35 @@ int main(int argc, char const *argv[])
       cudaEventCreate(&stop);
       for(int i=0; i<outrepeat; i++)
       {
-        WMMAF16TensorCore<<<sm_count,32*num_warps>>>(d_A, d_B, d_C, tilesC, repeat);
+        WMMAF16TensorCore<<<sm_count,32*num_warps>>>(d_A, d_B, d_C, tilesC);
       }
       cudaEventRecord(start);
       for(int i=0; i<outrepeat; i++)
       {
-        WMMAF16TensorCore<<<sm_count,32*num_warps>>>(d_A, d_B, d_C, tilesC, repeat);
+        WMMAF16TensorCore<<<sm_count,32*num_warps>>>(d_A, d_B, d_C, tilesC);
       }
       cudaEventRecord(stop);
       cudaEventSynchronize(stop);
       cudaEventElapsedTime(&milliseconds, start, stop);
       printf("[+] GPU(with Tensor Cores) Elapsed Time: %f ms\n", milliseconds);
-      printf("[+] TFLOPS: %.2f\n", ((float)M*tilesC*K*num_warps *2)*repeat*outrepeat / milliseconds / 1e9);
-  
+      printf("[+] TFLOPS: %.2f\n", ((float)M*N*K*sm_count*ILP*num_warps )*1024*2*outrepeat / milliseconds / 1e9);
     }
   }
   nvmlShutdown();
 
   cudaMemcpy(h_C, d_C, sizeofrmem, cudaMemcpyDeviceToHost);
-
+  int counter=0;
+  for(int i=0; i<8*num_warps; i++)
+  {
+    for(int j=0; j<tilesC; j++)
+    {
+      if(h_C[(i)*tilesC+j]!=0)
+      {
+        counter++;
+      }  //printf("%d,%d:%f\t",i,j,h_C[(i)*tilesC+j]);
+    }
+    // printf("\n");
+  }
+  printf("%d/%d\n",counter,num_warps*tilesC*8);
   return 0;
 }
